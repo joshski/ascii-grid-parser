ed7688fffb5117bf1928e895395a628797f9d925
joshski <joshuachisholm@gmail.com>
2011-05-26 16:40:35 +0100

    parses long char strins

--- spec.rb
+++ spec.rb
@@ -1,28 +1,28 @@
 class Cell
+  attr_accessor :adjacent_cells, :contents
 
-  def initialize(char,adjacent_cells=[])
-    @char = char
-    @adjacent_cells = adjacent_cells
-  end
-  
-  def adjacent_cells
-    @adjacent_cells
-  end
-  
-  def contents
-    @char
+  def initialize(char)
+    @contents = char
+    @adjacent_cells = []
   end
   
 end
 
 class AsciiGrid
   
-  def initialize(chars)
-    @chars = chars
+  def initialize(string)
+    chars = string.split("")
+    previous_cell = @origin = Cell.new(chars.shift)
+    chars.each do |char| 
+      new_cell = Cell.new(char)
+      new_cell.adjacent_cells << previous_cell
+      previous_cell.adjacent_cells << new_cell
+      previous_cell = new_cell
+    end
   end
   
   def origin
-    Cell.new(@chars, Array.new(@chars.length - 1))
+    @origin
   end
   
 end
@@ -36,9 +36,14 @@ describe AsciiGrid do
     it "originates in a cell with no adjacent cells" do
       grid.origin.should have(0).adjacent_cells
     end
-      
+
     it "originates in a cell containing 'S'" do
       grid.origin.contents.should == 'S'
+    end
+    
+    it "originates in a cell whose contents can be updated" do
+      grid.origin.contents = 'Z'
+      grid.origin.contents.should == 'Z'
     end
   end
   
@@ -66,5 +71,32 @@ describe AsciiGrid do
       grid.origin.should have(1).adjacent_cells
     end
     
+    it "originates in a cell containing 'S'" do
+      grid.origin.contents.should == 'S'
+    end
+    
+    it "originates in a cell adjacent to a cell containing 'R'" do
+      grid.origin.adjacent_cells.first.contents.should == 'R'
+    end
+    
+    it "originates in a cell which is adjacent to a cell which has 1 adjacent cell" do
+      grid.origin.adjacent_cells.first.should have(1).adjacent_cells
+    end
+    
+    it "originates in a cell which is adjacent to the origin" do
+      grid.origin.contents = 'X'
+      grid.origin.adjacent_cells.first.adjacent_cells.first.contents.should == 'X'
+    end
+    
+  end
+  
+  describe "when the source is an 'ABC'" do
+    let :grid do
+      AsciiGrid.new 'ABC'
+    end
+
+    it "has a middle cell with 2 adjacent cells" do
+      grid.origin.adjacent_cells.first.should have(2).adjacent_cells
+    end
   end
 end
