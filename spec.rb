27f9ccfac7f8fc28292a1107a38d5576daad4d40
joshski <joshuachisholm@gmail.com>
2011-05-26 16:59:33 +0100

    refactor

--- spec.rb
+++ spec.rb
@@ -27,68 +27,80 @@ class AsciiGrid
   
 end
 
-describe AsciiGrid do
-  describe "when the source is a 'S'" do
-    let :grid do
-      AsciiGrid.new 'S'
-    end
-
-    it "originates in a cell with no adjacent cells" do
-      grid.origin.should have(0).adjacent_cells
-    end
-
-    it "originates in a cell containing 'S'" do
-      grid.origin.contents.should == 'S'
-    end
-    
-    it "originates in a cell whose contents can be updated" do
-      grid.origin.contents = 'Z'
-      grid.origin.contents.should == 'Z'
-    end
+shared_examples_for "a grid with a single letter" do
+  let :grid do
+    AsciiGrid.new single_letter
   end
   
-  describe "when the source is a 'T" do
-    let :grid do
-      AsciiGrid.new 'T'
-    end
-
-    it "originates in a cell with no adjacent_cells" do
-      grid.origin.should have(0).adjacent_cells
-    end
-      
-    it "originates in a cell containing 'T'" do
-      grid.origin.contents.should == 'T'
-    end
-    
+  it "originates in a cell with no adjacent cells" do
+    grid.origin.should have(0).adjacent_cells
   end
 
-  describe "when the source is an 'SR'" do
-    let :grid do
-      AsciiGrid.new 'SR'
+  it "originates in a cell containing the single letter" do
+    grid.origin.contents.should == single_letter
+  end
+  
+  it "originates in a cell whose contents can be updated" do
+    grid.origin.contents = 'Z'
+    grid.origin.contents.should == 'Z'
+  end
+end
+
+shared_examples_for "a grid with two letters" do
+  let :grid do
+    AsciiGrid.new two_letters
+  end
+  
+  it "originates in a cell with one adjacent cell" do
+    grid.origin.should have(1).adjacent_cells
+  end
+  
+  it "originates in a cell containing the first letter" do
+    grid.origin.contents.should == two_letters.chars.first
+  end
+  
+  it "originates in a cell adjacent to a cell containing the second letter" do
+    grid.origin.adjacent_cells.first.contents.should == two_letters.split('').last
+  end
+  
+  it "originates in a cell which is adjacent to a cell which has 1 adjacent cell" do
+    grid.origin.adjacent_cells.first.should have(1).adjacent_cells
+  end
+  
+  it "originates in a cell which is adjacent to the origin" do
+    grid.origin.contents = 'X'
+    grid.origin.adjacent_cells.first.adjacent_cells.first.contents.should == 'X'
+  end
+  
+end
+
+describe AsciiGrid do
+  describe "when the source is 'S'" do
+    let :single_letter do
+      'S'
+    end
+
+    it_should_behave_like "a grid with a single letter"
+  end
+  
+  describe "when the source is 'T" do
+    let :single_letter do
+      'T'
+    end
+
+    it_should_behave_like "a grid with a single letter"
+  end
+
+  describe "when the source is 'SR'" do
+    let :two_letters do
+      'SR'
     end
     
-    it "originates in a cell with one adjacent cell" do
-      grid.origin.should have(1).adjacent_cells
-    end
-    
-    it "originates in a cell containing 'S'" do
-      grid.origin.contents.should == 'S'
-    end
-    
-    it "originates in a cell adjacent to a cell containing 'R'" do
-      grid.origin.adjacent_cells.first.contents.should == 'R'
-    end
-    
-    it "originates in a cell which is adjacent to a cell which has 1 adjacent cell" do
-      grid.origin.adjacent_cells.first.should have(1).adjacent_cells
-    end
-    
-    it "originates in a cell which is adjacent to the origin" do
-      grid.origin.contents = 'X'
-      grid.origin.adjacent_cells.first.adjacent_cells.first.contents.should == 'X'
-    end
+    it_should_behave_like "a grid with two letters"
     
   end
+  
+
   
   describe "when the source is an 'ABC'" do
     let :grid do
